8579e29307aefd7f46699307e472e5093f5c47e4
mdshihabuddin78 <shihab110897@gmail.com>
2024-10-28 21:11:57 +0600

    My potfolio

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+/*------------------typing animation-------------- */
+var typed = new Typed (".typing",{
+    strings:["Full Stuck Web Developer","web Designer","Wordpress Developer","web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    Loop:true
+})
